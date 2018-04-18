24249407202b97e1b5a97a4b92f19d31fa48b536
yurlian <31257437+yurlian@users.noreply.github.com>
2018-04-17 22:14:26 -0300

    ejemplo de alta y listar

---
+++ estructuras2/empleado.h
@@ -0,0 +1,22 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+typedef struct {
+    int dia;
+    int mes;
+    int anio;
+
+}EFecha;
+
+typedef struct {
+    char nombre[50];
+    int legajo;
+    float salario;
+    EFecha fn;
+
+}EEmpleado;
+
+void alta(EEmpleado[], int);
+void listar(EEmpleado[], int);
+
+#endif // FUNCIONES_H_INCLUDED
